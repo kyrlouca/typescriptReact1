68d611587b4b0a11a73b142844da3e625d509eda
kyrlouca <kyrlouca@gmail.com>
2019-09-30 10:05:09 +0300

    ok

--- src/storage/order-reducer.tsx
+++ src/storage/order-reducer.tsx
@@ -2,28 +2,31 @@ export type TStateOrder = {
   custId: string;
   orderId: string;
   orderValue: number;
-  customerName: string;
+  customerName:string;
   dateCreated: string;
 };
 
-export type TStateShape = {
-  customerId: string;
+export type TStateShape= {
+  customerId:string;
   items: TStateOrder[];
-};
+}
 
-export type TpayLoad = TStateOrder[];
-export type TReducerAction = { type: string; payload: TStateOrder[] };
+export type TpayLoad =TStateOrder[];
+export type TReducerAction={ type: string; payload: TStateOrder[] }
 
-const initialState: TStateShape = { customerId: '', items: [] };
+const initialState : TStateShape={customerId:'', items:[]};
 
-export const orderReducer = (state: TStateShape, action: TReducerAction) => {
+export const orderReducer = (
+  state:TStateShape,action:TReducerAction
+) =>  {
+
   switch (
     action.type //action may not have type like redux
   ) {
     case 'RESET':
       return initialState;
     case 'FETCH':
-      const newShape = { ...state, items: [...action.payload] };
+      const newShape= {...state, items: [...action.payload]}
       return newShape;
     default:
       return state;
